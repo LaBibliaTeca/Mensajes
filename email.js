5210a2de39ce61018acd7a1109eba707b884722e
LaBibliaTeca <gustavo.uliarte@hotmail.com>
2024-09-03 03:58:28 -0300

    Update email.js

--- email.js
+++ email.js
@@ -1,23 +1,21 @@
-// Incluir la biblioteca de EmailJS
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://cdn.emailjs.com/dist/email.min.js';
-document.head.appendChild(script);
+ <!-- Incluir EmailJS SDK -->
+    <script type="text/javascript" src="https://cdn.emailjs.com/dist/email.min.js"></script>
+    <script type="text/javascript">
+        (function() {
+            emailjs.init("_vmfJzPaAfLKlio-k"); // Reemplaza con tu User ID de EmailJS
+        })();
+    </script>
 
-script.onload = function() {
-    // Iniciar EmailJS
-    emailjs.init("_vmfJzPaAfLKlio-k"); // Reemplaza "YOUR_PUBLIC_KEY" con tu clave pública
+    <script type="text/javascript">
+        document.getElementById('contact-form').addEventListener('submit', function(event) {
+            event.preventDefault(); // Previene la acción por defecto del formulario
 
-    // Manejar el envío del formulario
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();  // Previene la recarga de la página
-
-        // Enviar el formulario usando EmailJS
-        emailjs.sendForm('service_2ajarrr', 'template_biqdvfi', this)
-            .then(function() {
-                alert('Correo enviado exitosamente!');
-            }, function(error) {
-                alert('Error al enviar el correo: ' + JSON.stringify(error));
-            });
-    });
-};
+            // Enviar el formulario usando EmailJS
+            emailjs.sendForm('service_2ajarrr', 'template_biqdvfi', this)
+                .then(function() {
+                    alert('Correo enviado exitosamente!');
+                }, function(error) {
+                    alert('Error al enviar el correo: ' + JSON.stringify(error));
+                });
+        });
+    </script>
