b40b0c76f35efffce1dfeb59599a33d4d1d8af07
LaBibliaTeca <gustavo.uliarte@hotmail.com>
2024-09-03 03:15:47 -0300

    Update email.js

--- email.js
+++ email.js
@@ -1,17 +1,23 @@
-// Incluir EmailJS SDK
-(function() {
+// Incluir la biblioteca de EmailJS
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdn.emailjs.com/dist/email.min.js';
+document.head.appendChild(script);
+
+script.onload = function() {
+    // Iniciar EmailJS
     emailjs.init("_vmfJzPaAfLKlio-k"); // Reemplaza "YOUR_PUBLIC_KEY" con tu clave pública
-})();
 
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();  // Previene la recarga de la página
+    // Manejar el envío del formulario
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();  // Previene la recarga de la página
 
-    // Enviar el formulario usando EmailJS 
-    emailjs.sendForm('service_2ajarrr', 'template_biqdvfi', this)
-        .then(function() {
-            alert('Correo enviado exitosamente!');
-        }, function(error) {
-            alert('Error al enviar el correo: ' + JSON.stringify(error));
-        });
-});
-
+        // Enviar el formulario usando EmailJS
+        emailjs.sendForm('service_2ajarrr', 'template_biqdvfi', this)
+            .then(function() {
+                alert('Correo enviado exitosamente!');
+            }, function(error) {
+                alert('Error al enviar el correo: ' + JSON.stringify(error));
+            });
+    });
+};
